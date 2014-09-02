0c0709b8e4d82e45ad6741aa93692642810fda9a
DraganVel <radosevo@yahoo.com>
2014-09-01 22:04:05 -0400

    barcode code copied from wildbeast

--- www/js/index.js
+++ www/js/index.js
@@ -34,8 +34,9 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
 
-        cordova.plugins.barcodeScanner.scan(
-            function (result) {
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+        scanner.scan( function (result) {
                 alert("We got a barcode\n" +
                     "Result: " + result.text + "\n" +
                     "Format: " + result.format + "\n" +
